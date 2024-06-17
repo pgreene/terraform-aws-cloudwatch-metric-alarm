802aa753751e8659fb17d7676f8f0f1865f7645d
pgreene <pgreene@pgreenes-MacBook-Pro.local>
2024-06-17 17:16:07 -0400

    Updated terraform aws cloudwatch metric alarm config

--- cloudwatch-metric-alarm-vars.tf
+++ cloudwatch-metric-alarm-vars.tf
@@ -1,59 +1,90 @@
 variable "name" {
-  description = "use naming module https://github.com/Voyanta/terraform-terraform-label"
+  description = "(Required) The descriptive name for the alarm. This name must be unique within the AWS account"
   default = ""
 }
 
-variable "tags" {
-  default = {}
-}
-
-variable "severity" {
-  default = "sev2"
-}
-
 variable "comparison_operator" {
+  description = "(Required) The arithmetic operation to use when comparing the specified Statistic and Threshold. The specified Statistic value is used as the first operand. Either of the following is supported: GreaterThanOrEqualToThreshold, GreaterThanThreshold, LessThanThreshold, LessThanOrEqualToThreshold. Additionally, the values LessThanLowerOrGreaterThanUpperThreshold, LessThanLowerThreshold, and GreaterThanUpperThreshold are used only for alarms based on anomaly detection models."
   default = "LessThanOrEqualToThreshold"
 }
 
 variable "evaluation_periods" {
+  description = "(Required) The number of periods over which data is compared to the specified threshold."
   default = "3"
 }
 
 variable "period" {
+  description = "(Optional) The period in seconds over which the specified statistic is applied."
   default = "900"
 }
 
 variable "statistic" {
+  description = "(Optional) The statistic to apply to the alarm's associated metric. Either of the following is supported: SampleCount, Average, Sum, Minimum, Maximum"
   default = "Sum"
 }
 
 variable "threshold" {
+  description = "(Optional) The value against which the specified statistic is compared. This parameter is required for alarms based on static thresholds, but should not be used for alarms based on anomaly detection models."
   default = "3"
 }
 
+variable "threshold_metric_id" {
+  description = "(Optional) If this is an alarm based on an anomaly detection model, make this value match the ID of the ANOMALY_DETECTION_BAND function."
+  default = null
+}
+
+variable "actions_enabled" {
+  description = "(Optional) Indicates whether or not actions should be executed during any changes to the alarm's state. Defaults to true."
+  default = null
+}
+
 variable "treat_missing_data" {
+  description = "(Optional) Sets how this alarm is to handle missing data points. The following values are supported: missing, ignore, breaching and notBreaching. Defaults to missing."
   default = "missing"
 }
 
 variable "metric_name" {
+  description = "(Optional) The name for the alarm's associated metric. See docs for supported metrics."
   default = "successful"
 }
 
 variable "metric_namespace" {
+  description = "(Optional) The namespace for the alarm's associated metric. See docs for the list of namespaces. See docs for supported metrics."
   default = "uptime"
 }
 
+variable "metric_query" {
+  description = "(Optional) Enables you to create an alarm based on a metric math expression. You may specify at most 20."
+  default = null
+}
+
 variable "alarm_description" {
-  default = "Alarming based off metric from uptime stats"
+  default = "Cloudwatch Metric Alarm created by Terraform."
+}
+
+variable "datapoints_to_alarm" {
+  description = "(Optional) The number of datapoints that must be breaching to trigger the alarm."
+  default = null
+}
+
+variable "extended_statistic" {
+  description = "(Optional) The percentile statistic for the metric associated with the alarm. Specify a value between p0.0 and p100."
+  default = null
+}
+
+variable "evaluate_low_sample_count_percentiles" {
+  description = "(Optional) Used only for alarms based on percentiles. If you specify ignore, the alarm state will not change during periods with too few data points to be statistically significant. If you specify evaluate or omit this parameter, the alarm will always be evaluated and possibly change state no matter how many data points are available. The following values are supported: ignore, and evaluate."
+  default = null
 }
 
 variable "insufficient_data_actions" {
-  default = []
+  description = "(Optional) The list of actions to execute when this alarm transitions into an INSUFFICIENT_DATA state from any other state. Each action is specified as an Amazon Resource Name (ARN)."
+  default = null
 }
 
 variable "alarm_actions" {
   description = "(Optional) The list of actions to execute when this alarm transitions into an ALARM state from any other state. Each action is specified as an Amazon Resource Name (ARN)."
-  default = ""
+  default = null
 }
 
 variable "ok_actions" {
@@ -62,9 +93,18 @@ variable "ok_actions" {
 }
 
 variable "dimensions" {
+  description = "(Optional) The dimensions for the alarm's associated metric. For the list of available dimensions see the AWS documentation here."
   default = null
 }
 
+variable "unit" {
+  description = "(Optional) The unit for the alarm's associated metric."
+  default = null
+}
+
+variable "tags" {
+  default = {}
+}
 
 /*
 
--- cloudwatch-metric-alarm.tf
+++ cloudwatch-metric-alarm.tf
@@ -1,22 +1,24 @@
 resource "aws_cloudwatch_metric_alarm" "general" {
-  //for_each                  = var.elb_list
-  //alarm_name                = join("-",[var.severity,"5xx",each.key])
   alarm_name = var.name 
   metric_name = var.metric_name
   namespace = var.metric_namespace
   alarm_description = var.alarm_description
+  datapoints_to_alarm = var.datapoints_to_alarm
   comparison_operator = var.comparison_operator
   period = var.period
   evaluation_periods = var.evaluation_periods
   threshold = var.threshold
+  threshold_metric_id = var.threshold_metric_id
+  actions_enabled = var.actions_enabled
   statistic = var.statistic
+  extended_statistic = var.extended_statistic
   treat_missing_data = var.treat_missing_data
-  
-  insufficient_data_actions = []
+  evaluate_low_sample_count_percentiles = var.evaluate_low_sample_count_percentiles
+  insufficient_data_actions = var.insufficient_data_actions
   dimensions = var.dimensions
-
+  #metric_query = var.metric_query
   alarm_actions = var.alarm_actions
   ok_actions = var.ok_actions
-
+  unit = var.unit
   tags = var.tags
 }
